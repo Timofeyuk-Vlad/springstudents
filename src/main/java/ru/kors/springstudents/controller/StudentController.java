531db42ab79919ca47530019ce5c23d2c8b15373
Timofeyuk-Vlad <vladtimofeyuk@gmail.com>
2025-02-24 13:52:37 +0300

    .

--- src/main/java/ru/kors/springstudents/controller/StudentController.java
+++ src/main/java/ru/kors/springstudents/controller/StudentController.java
@@ -2,6 +2,7 @@ package ru.kors.springstudents.controller;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
