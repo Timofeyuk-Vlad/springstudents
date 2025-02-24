f0cd9c43d2616541a6ff90b675f05e6050fc725f
Timofeyuk-Vlad <vladtimofeyuk@gmail.com>
2025-02-24 13:34:45 +0300

    .

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.3"
+	id("io.spring.dependency-management") version "1.1.7"
+
+	checkstyle
+	id("org.sonarqube") version "6.0.1.5171"
+}
+
+group = "ru.kors"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+checkstyle {
+	toolVersion = "10.21.2"
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "Timofeyuk-Vlad_springstudents")
+		property("sonar.organization", "timofeyuk-vlad")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
