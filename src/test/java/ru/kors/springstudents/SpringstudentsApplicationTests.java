30b8b50b1370537e1bc6754a0ce8e4277773a519
Timofeyuk-Vlad <vladtimofeyuk@gmail.com>
2025-02-28 01:40:32 +0300

    .

--- src/test/java/ru/kors/springstudents/SpringstudentsApplicationTests.java
+++ src/test/java/ru/kors/springstudents/SpringstudentsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringStudentsApplicationTests {
+public class SpringstudentsApplicationTests {
 
     @Test
     void contextLoads() {
