76f0836e9ad024c202fd956a09c16cc9f60dc164
Timofeyuk-Vlad <vladtimofeyuk@gmail.com>
2025-02-27 12:59:25 +0300

    Sas

--- src/test/java/ru/kors/springstudents/SpringstudentsApplicationTests.java
+++ src/test/java/ru/kors/springstudents/SpringstudentsApplicationTests.java
@@ -1,14 +1,13 @@
 package ru.kors.springstudents;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class SpringStudentsApplicationTests {
-//
-//@Test
-//void contextLoads() {
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentsApplicationTests {
+
+@Test
+void contextLoads() {
 //no tests at this point
-//}
-//
-//}
+}
+}
