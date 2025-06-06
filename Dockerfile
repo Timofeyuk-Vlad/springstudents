4c7933c215ef644d0858e556465cdc3eb7e12bc8
Timofeyuk-Vlad <vladtimofeyuk@gmail.com>
2025-06-06 06:44:28 +0300

    Final

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM openjdk:17-jdk-alpine
-RUN apk add --no-cache bash
+RUN apk add --no-cache bash coreutils
 WORKDIR /app
 COPY gradle/ gradle/
 COPY gradlew .
 COPY build.gradle.kts .
 COPY settings.gradle.kts .
 COPY src/ src/
+COPY config/checkstyle/ config/checkstyle/
 RUN chmod +x ./gradlew
-RUN ./gradlew build --no-daemon
-COPY build/libs/*.jar app.jar
+RUN ./gradlew bootJar --no-daemon
+RUN ./gradlew build && \
+    cp build/libs/springstudents-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "app.jar" ]
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/ru/kors/springstudents/SpringStudentsApplication.java
+++ src/main/java/ru/kors/springstudents/SpringStudentsApplication.java
@@ -2,13 +2,26 @@ package ru.kors.springstudents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 @EnableAsync
 public class SpringStudentsApplication {
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringStudentsApplication.class, args);
     }
+
+     @Bean
+         // Раскомментируй для проверки
+     CommandLineRunner runner() {
+         return args -> {
+             System.out.println("!!!!!!!!!!!! DB PASSWORD: " + dbPassword);
+         };
+     }
 }
